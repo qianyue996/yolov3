1b13030d88e75b96d9b622805230d977445aa476
qianyue996 <3010875645@qq.com>
2025-04-24 10:17:44 +0800

    update

--- nets/yolo_loss.py
+++ nets/yolo_loss.py
@@ -10,10 +10,12 @@ class YOLOv3LOSS():
         self.IMG_SIZE = CONF.imgsize
         self.anchors  = CONF.anchors
         self.classes_num = len(CONF.class_name)
+        self.stride = CONF.sample_ratio
 
-        self.lambda_1 = 0.4
-        self.lambda_2 = 1
-        self.lambda_3 = 0.4
+        self.conf_lambda = [0.4, 1.0, 4]
+        self.loc_lambda = 0.05
+        self.obj_lambda = 5
+        self.cls_lambda = 1
     def BCELoss(self, x, y):
         eps = 1e-7
         x = torch.clamp(x, eps, 1 - eps)
@@ -40,88 +42,40 @@ class YOLOv3LOSS():
 
             h = prediction[..., 3]
 
-            y_true = self.build_target(targets, anchors, S)
+            y_true = self.build_target(B, S, stride, targets, anchors)
 
             obj_mask = y_true[..., 4] == 1
             noobj_mask = ~obj_mask
             
-            self.new_function(x, y, w, h, obj_mask, anchors)
-
             n = obj_mask.sum().item()
             if n != 0:
-                
 
-
-
-
-
-                x = (torch.sigmoid(prediction[..., 0])[obj_mask] + i_x) * stride
-                t_x = target[..., 0][obj_mask] + i_j[..., 0][obj_mask] * stride
-
-                y = (torch.sigmoid(prediction[..., 1][obj_mask]) + i_y) * stride
-                t_y = target[..., 1][obj_mask] + i_j[..., 0][obj_mask] * stride
-
-                w = torch.exp(prediction[..., 2][obj_mask]) * anchors[b_n.long(), 0]
-                t_w = torch.exp(target[..., 2][obj_mask]) * anchors[b_n.long(), 0]
-
-                h = torch.exp(prediction[..., 3][obj_mask]) * anchors[b_n.long(), 1]
-                t_h = torch.exp(target[..., 3][obj_mask]) * anchors[b_n.long(), 1]
-
-                c = torch.sigmoid(prediction[..., 4][obj_mask])
-                t_c = target[..., 4][obj_mask]
+                c = torch.sigmoid(prediction[..., 4])
+                t_c = y_true[..., 4]
 
                 _cls = torch.sigmoid(prediction[..., 5:][obj_mask])
-                t_cls = target[..., 5:][obj_mask]
+                t_cls = y_true[..., 5:][obj_mask]
 
-                # noobj target
-                no_conf = torch.sigmoid(prediction[..., 4][noobj_mask])
-                no_t_conf = torch.zeros_like(no_conf)
-                noobj_loss = self.BCELoss(no_conf, no_t_conf).mean()
-                
-                # giou
-                x1 = x - w / 2
-                t_x1 = t_x - t_w / 2
-
-                y1 = y - h / 2
-                t_y1 = t_y - t_h / 2
-
-                x2 = x + w / 2
-                t_x2 = t_x + t_w / 2
-
-                y2 = y + h / 2
-                t_y2 = t_y + t_h / 2
-
-                gt_box = torch.stack([x1, y1, x2, y2], dim=1)
-                t_gt_box = torch.stack([t_x1, t_y1, t_x2, t_y2], dim=1)
-                giou = self.compute_giou(gt_box, t_gt_box)
-
+                giou = self.new_function(x, y, w, h, obj_mask, anchors)
                 loss_loc = (1 - giou).mean()
-                loss_conf = self.BCELoss(c, t_c).mean()
-                loss_cls = self.BCELoss(_cls, t_cls).mean(dim=0).sum()
-                
-                # 这里加权
-                if i == 0:  # 第一个尺度（26x26）
-                    loss += self.lambda_1 * (noobj_loss + loss_loc + loss_conf + loss_cls)
-                elif i == 1:  # 第二个尺度（52x52）
-                    loss += self.lambda_2 * (noobj_loss + loss_loc + loss_conf + loss_cls)
-                elif i == 2:  # 第三个尺度（13x13）
-                    loss += self.lambda_3 * (noobj_loss + loss_loc + loss_conf + loss_cls)
 
-                writer.add_scalar(f'noobj_feture: {S}', noobj_loss, global_step)
-                writer.add_scalar(f'xywh_feture: {S}', loss_loc, global_step)
-                writer.add_scalar(f'conf_feture: {S}', loss_conf, global_step)
-                writer.add_scalar(f'cls_feture: {S}', loss_cls, global_step)
+                loss_cls = self.BCELoss(_cls, t_cls).mean()
+                
+                loss += loss_loc * self.loc_lambda + loss_cls * self.cls_lambda
+            
+            loss_conf = self.BCELoss(c, t_c)[obj_mask | noobj_mask].mean()
+
+            loss += loss_conf * self.conf_lambda[i] * self.obj_lambda
 
         return loss
 
-    def build_target(self, targets, anchors, S, thre=0.4):
-        B = len(targets)
+    def build_target(self, B, S, stride, targets, anchors, thre=0.4):
         y_true = torch.zeros(B, 3, S, S, 5 + 80, device=self.device)
 
         for bs in range(B):
             batch_target = torch.zeros_like(targets[bs])
-            batch_target[:, 0:4] = targets[bs][:, 0:4] * S
-            batch_target[:, 2:4] = batch_target[:, 2:4] * self.IMG_SIZE
+            batch_target[:, 0:2] = targets[bs][:, 0:2] * S
+            batch_target[:, 2:4] = targets[bs][:, 2:4] * self.IMG_SIZE
             batch_target[:, 4] = targets[bs][:, 4]
 
             gt_box = batch_target[:, 2:4]
@@ -185,18 +139,29 @@ class YOLOv3LOSS():
         giou = iou - (area_c - union) / area_c.clamp(min=1e-6)
         return giou
 
-    def new_function(self, x, y, w, h, obj_mask, anchors):
+    def new_function(self, x, y, w, h, obj_mask, anchors, stride):
         best_a = obj_mask.nonzero()[:, 1]
         grid_x = obj_mask.nonzero()[:, 2]
         grid_y = obj_mask.nonzero()[:, 3]
 
-        x = (x[obj_mask] + grid_x) * self.IMG_SIZE
-        y = (y[obj_mask] + grid_y) * self.IMG_SIZE
+        x = (x[obj_mask] + grid_x) * stride
+        y = (y[obj_mask] + grid_y) * stride
         w = torch.exp(w[obj_mask]) * anchors[best_a][:, 0]
         h = torch.exp(h[obj_mask]) * anchors[best_a][:, 1]
 
-        x1 = x - w[obj_mask] / 2
-        y1 = y[obj_mask] - h[obj_mask] / 2
-        x2 = x[obj_mask] + w[obj_mask] / 2
-        y2 = y[obj_mask] + h[obj_mask] / 2
-        pass
+        x1 = torch.clamp(x - w / 2, min=1e-6, max=self.IMG_SIZE)
+        y1 = torch.clamp(y - h / 2, min=1e-6, max=self.IMG_SIZE)
+        x2 = torch.clamp(x + w / 2, min=1e-6, max=self.IMG_SIZE)
+        y2 = torch.clamp(y + h / 2, min=1e-6, max=self.IMG_SIZE)
+
+        t_x1 = torch.clamp(x - anchors[best_a][:, 0] / 2, min=1e-6, max=self.IMG_SIZE)
+        t_y1 = torch.clamp(y - anchors[best_a][:, 1] / 2, min=1e-6, max=self.IMG_SIZE)
+        t_x2 = torch.clamp(x + anchors[best_a][:, 0] / 2, min=1e-6, max=self.IMG_SIZE)
+        t_y2 = torch.clamp(y + anchors[best_a][:, 1] / 2, min=1e-6, max=self.IMG_SIZE)
+
+        pred_box = torch.stack([x1, y1, x2, y2], dim=-1)
+        targ_box = torch.stack([t_x1, t_y1, t_x2, t_y2], dim=-1)
+
+        giou = self.compute_giou(pred_box, targ_box)
+        
+        return giou
