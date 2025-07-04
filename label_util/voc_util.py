ede0ae5cc67c06473230304b6f8ca5320747cefc
qianyue996 <3010875645@qq.com>
2025-07-04 08:14:56 +0800

    feat: update

---
+++ label_util/voc_util.py
@@ -0,0 +1,68 @@
+import json
+import os
+import xml.etree.ElementTree as ET
+import random
+import yaml
+from tqdm import tqdm
+
+def main(cfg, out_train, out_val):
+    big_labels_data = []
+    name2id = {n: i for i, n in enumerate(cfg['voc']['class_name'])}
+
+    for annotation_path in [cfg['voc']['annotation_path_2007'], cfg['voc']['annotation_path_2012']]:
+        for file_name in tqdm(os.listdir(annotation_path)):
+            xml_path = os.path.join(annotation_path, file_name)
+            img_path = os.path.join(annotation_path.replace('Annotations', 'JPEGImages'), file_name.split(".")[0] + ".jpg")
+            single_data = {
+                "file_path": img_path,
+                "labels": []
+            }
+            root = ET.parse(xml_path).getroot()
+            for obj in root.iter("object"):
+                obj_name = obj.find("name").text.strip() # type: ignore
+                bndbox = obj.find("bndbox")
+                box_part = ["xmin", "ymin", "xmax", "ymax"]
+                label = {
+                    "name_id": name2id[obj_name]
+                }
+                for part in box_part:
+                    label[part] = bndbox.find(part).text # type: ignore
+                single_data["labels"].append(label)
+            big_labels_data.append(single_data)
+
+    # 打乱数据，拆分为训练集和验证集
+    random.shuffle(big_labels_data)
+    split_index = int(len(big_labels_data) * cfg['ds']['split_ratio'])
+    train_data = big_labels_data[:split_index]
+    val_data = big_labels_data[split_index:]
+
+    with open(out_train, 'w', encoding='utf-8') as f:
+        train_json = json.dumps(train_data, ensure_ascii=False, indent=2)
+        f.write(train_json)
+
+    with open(out_val, 'w', encoding='utf-8') as f:
+        val_json = json.dumps(val_data, ensure_ascii=False, indent=2)
+        f.write(val_json)
+
+
+if __name__ == "__main__":
+    out_train = 'voc_train.json'
+    out_val = 'voc_val.json'
+    with open('config/yolo_conf.yaml', encoding="ascii", errors="ignore")as f:
+        cfg = yaml.safe_load(f)
+    main(cfg, out_train, out_val)
+
+"""type for yolo label
+{
+    "file_path": /path/to/file.jpg/png,
+    "labels": [
+        {
+            "name_id": int,
+            "xmin": xxx,
+            "ymin": xxx,
+            "xmax": xxx,
+            "ymax": xxx,
+        }
+    ]
+}
+"""
